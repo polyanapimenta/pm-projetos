1c77fcaeb4e6b142c015f5e6e9a52e49220b77cd
Polyana Pimenta <polyana.pimenta@gmail.com>
2017-10-22 18:01:47 -0200

    Atualizando o script de automacao de commits

--- atualizacao-repositorio.bat
+++ atualizacao-repositorio.bat
@@ -1,6 +1,10 @@
+@echo off
 cd "C:\Users\Polyana\Documents\Projetos\pm-projetos" 
 git pull origin master
+git status
 git add .
+git status
+echo Insira a mensagem do seu Commit!
 set /p input=
 git commit -m %input%
 git push origin master
