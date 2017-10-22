55fcc77ec0b3cb70258d6c1d97715c2d4e94a70d
Polyana Pimenta <polyana.pimenta@gmail.com>
2017-10-22 13:25:37 -0200

    Inicializando/Modificando arquivos no repositorio

---
+++ atualizacao-repositorio.bat
@@ -0,0 +1,7 @@
+cd "C:\Users\Polyana\Documents\Projetos\pm-projetos" 
+git status
+git add .
+git commit -m 'Inserindo-Modificando arquivos no repositorio'
+git status
+git pull origin master
+git push origin master
