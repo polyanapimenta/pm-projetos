1d478dd7e0abd5deb9d22b6248291645783e37a8
Polyana Pimenta <polyana.pimenta@gmail.com>
2017-10-22 18:35:42 -0200

    Melhorando atualizacao-repositorio

--- atualizacao-repositorio.bat
+++ atualizacao-repositorio.bat
@@ -1,10 +1,24 @@
 @echo off
 cd "C:\Users\Polyana\Documents\Projetos\pm-projetos" 
+echo --------------------------------------------------    
+echo ** ATUALIZANDO O REPOSITORIO LOCAL...
+echo --------------------------------------------------
 git pull origin master
+echo --------------------------------------------------
+echo ** ARQUIVOS NO REPOSITORIO:
+echo --------------------------------------------------
 git status
+echo --------------------------------------------------  
+echo ** ADD ARQUIVOS MODIFICADOS...
+echo --------------------------------------------------
 git add .
 git status
-echo Insira a mensagem do seu Commit!
+echo --------------------------------------------------    
+echo ** INSIRA A MENSAGEM PARA O COMMIT!
+echo --------------------------------------------------
 set /p input=
 git commit -m %input%
+echo --------------------------------------------------
+echo ** COMMIT REALIZADO.. UPPANDO AQUIVOS...
+echo --------------------------------------------------
 git push origin master
