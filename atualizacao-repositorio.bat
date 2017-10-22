4130148e22f30c314161935bc0e9ff0e3e2eb481
Polyana Pimenta <polyana.pimenta@gmail.com>
2017-10-22 13:57:13 -0200

    Modificando arquivos no repositorio

--- atualizacao-repositorio.bat
+++ atualizacao-repositorio.bat
@@ -1,5 +1,5 @@
 cd "C:\Users\Polyana\Documents\Projetos\pm-projetos" 
 git pull origin master
 git add .
-git commit -m "Inicializando/Modificando arquivos no repositorio"
+git commit -m "Modificando arquivos no repositorio"
 git push origin master
